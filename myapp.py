9548408155580a645ab0d23a6ed1fc7ab22ba27b
Nicolas Bueno Emidio <nicolasbueno.emidio@gmail.com>
2024-05-18 15:40:45 -0300

    teste sem barra lateral

--- myapp.py
+++ myapp.py
@@ -1,7 +1,7 @@
 # Importando as bibliotecas necessárias
 import openai
 import streamlit as st
-from streamlit_option_menu import option_menu
+#from streamlit_option_menu import option_menu
 import pandas as pd
 import time
 import shelve
